64e970020de8d31fa4009704c0ff6e9c18859e84
anniewu <annie@hyweb.com.tw>
2023-11-06 13:42:57 +0800

    20231106新增燈箱樣式

--- js/customize.js
+++ js/customize.js
@@ -1,2 +1,16 @@
 // 自行加入的JS請寫在這裡
-$(function () {});
+$(function () {
+  //書籍資料書籍資料
+  $('.bookinformation_btn').click(function () {
+    $('#bookinformation').fadeIn();
+    $('body').addClass('fixed');
+  });
+  $('.lightboxstyle_block .close').click(function () {
+    $('#bookinformation').fadeOut();
+    $('body').removeClass('fixed');
+  });
+  $('.overlay').click(function () {
+    $('#bookinformation').fadeOut();
+    $('body').removeClass('fixed');
+  });
+});
